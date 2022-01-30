46cb08760256d7e81ce06455570b5c5e6f5f4f0e
Gitanshu <gitanshugupta7@gmail.com>
2022-01-30 14:47:57 +0530

    added email

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function sendEmail() {
     Email.send({
         SecureToken : "013bcbbc-02c4-4136-b93f-fd099827470a",
-        To : 'them@website.com',
-        From : "you@isp.com",
+        To : 'gitanshugupta7@gmail.com',
+        From : "gitanshuaptitute@gmail.com",
         Subject : "This is the subject",
         Body : "And this is the body"
     }).then(
