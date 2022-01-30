37436954f495c380ad6110068b68961f1a45ca2e
Gitanshu <gitanshugupta7@gmail.com>
2022-01-30 15:31:57 +0530

    added

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-//index.js  
-function sendEmail() {
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "gitanshuaptitute",
-        Password : "Qwerty@9182",
-        To : 'gitanshugupta7@gmail.com',
-        From : "gitanshugupta7@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
+
 
 
