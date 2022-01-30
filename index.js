437769b8ebd8583d9f72e40773f19179b9dbf8a5
Gitanshu <gitanshugupta7@gmail.com>
2022-01-30 23:32:34 +0530

    removed

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-
+function sendEmail() {
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "gitanshuaptitute@gmail.com",
+      Password: "Qwerty@9182",
+      To: 'gitanshuaptitute@gmail.com',
+      From: "gitanshuaptitute@gmail.com",
+      Subject: "Sending Email using javascript",
+      Body: "Well that was easy!!",
+    })
+      .then(function (message) {
+        alert("mail sent successfully")
+      });
+  }
 
 
