2e94b312b558356f88d1f113fcb0617f9e50b5b9
Gitanshu <gitanshugupta7@gmail.com>
2022-02-01 14:21:30 +0530

    added contactus

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function sendEmail() {
       Host: "smtp.gmail.com",
       Username: "gitanshuaptitute@gmail.com",
       Password: "Qwerty@9182",
-      To: 'gitanshuaptitute@gmail.com',
-      From: "gitanshuaptitute@gmail.com",
+      To: 'gitanshugupta7@gmail.com',
+      From: "anything@gmail.com",
       Subject: "Sending Email using javascript",
       Body: "Well that was easy!!",
     })
-      .then(function (message) {
-        alert("mail sent successfully")
-      });
+     .then( console.log("Mail sent successfully") ).catch((error) => {
+        console.log(error);
+      })
   }
 
 
