503efa327026d2598ac08ec53b35e0d5e16a993e
Gitanshu <gitanshugupta7@gmail.com>
2022-02-01 19:16:07 +0530

    added contactus

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 function sendEmail() {
-    Email.send({
-      Host: "smtp.gmail.com",
-      Username: "gitanshuaptitute@gmail.com",
-      Password: "Qwerty@9182",
-      To: 'gitanshugupta7@gmail.com',
-      From: "anything@gmail.com",
-      Subject: "Sending Email using javascript",
-      Body: "Well that was easy!!",
-    })
-     .then( console.log("Mail sent successfully") ).catch((error) => {
-        console.log(error);
-      })
+  username = document.getElementById('username').value;
+  //email = document.getElementById('email').value;
+  phone = document.getElementById('phone').value;
+
+  console.log(`mailto:gitanshugupta7@gmail.com?subject=Appointment&body= ${username}`);
+  if(username != "" &&  phone != "")
+  {
+    window.open(`mailto:gitanshugupta7@gmail.com?subject=Appointment&body=I, ${username}, would like to make an appointment with you, please contact me using my phone number, ${phone}.`);
   }
+}
 
 
