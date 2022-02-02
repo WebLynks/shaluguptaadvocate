a32890067622481fd0aebafa3465a0cde60313c7
Gitanshu <gitanshugupta7@gmail.com>
2022-02-02 09:33:52 +0530

    added mail

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function sendEmail() {
   console.log(`mailto:gitanshugupta7@gmail.com?subject=Appointment&body= ${username}`);
   if(username != "" &&  phone != "")
   {
-    window.open(`mailto:gitanshugupta7@gmail.com?subject=Appointment&body=I, ${username}, would like to make an appointment with you, please contact me using my phone number, ${phone}.`);
+    window.open(`mailto:nikkigupta09@gmail.com?subject=Appointment&body=I, ${username}, would like to make an appointment with you, please contact me using my phone number, ${phone}.`);
   }
 }
 
